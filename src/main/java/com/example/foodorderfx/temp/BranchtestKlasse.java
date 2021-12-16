82eb963ae20a1351ecc6218704c076c5cac8117e
Franzi <eingaertner.gfi@gmail.com>
2021-12-16 13:22:08 +0100

    main methode hinzugefügt und mit Hello World getestet

--- src/main/java/com/example/foodorderfx/temp/BranchtestKlasse.java
+++ src/main/java/com/example/foodorderfx/temp/BranchtestKlasse.java
@@ -3,4 +3,8 @@ package com.example.foodorderfx.temp;
 public class BranchtestKlasse {
 
     private String attributus;
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
